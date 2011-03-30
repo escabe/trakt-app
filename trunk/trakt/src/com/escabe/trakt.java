447d39f8c8dccd8ca6c55f18ec6f3eeac71172dd
martijn.aben@gmail.com <martijn.aben@gmail.com@5425ca5d-d409-134a-e6a4-18aea6580cd7>
2011-03-30 19:38:30 +0000

    Added Settings Screen. Created username/password setting. Removed Testing. Implemented MyFlipper.

--- trunk/trakt/src/com/escabe/trakt.java
+++ trunk/trakt/src/com/escabe/trakt.java
@@ -15,9 +15,18 @@
  ******************************************************************************/
 package com.escabe;
 
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Stack;
+
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.AnimationUtils;
 import android.widget.TextView;
@@ -25,29 +34,92 @@ import android.widget.ViewFlipper;
 
 public class trakt extends Activity {
     /** Called when the activity is first created. */
-    public ViewFlipper vf;
+//    public ViewFlipper vf;
     private TraktList traktlist;
     private Search search;
+    
+    public MyFlipper myflipper;
+    
+    private SharedPreferences settings;
+    
+	public enum MyView {
+		MAIN_MENU(0), TRAKTLIST(1), SEARCH(2), DETAILS(3), SETTINGS(4);
+		private int id;
+		MyView(int i) {
+			id = i;
+		}
+		public int id() {
+			return id;
+		}
+	}
+	
+    public class MyFlipper {
+    	private ViewFlipper viewflipper;
+    	private Stack<Integer> viewstack;
+    	public MyFlipper(ViewFlipper vf, Activity parent) {
+    		viewflipper = vf;
+    		viewstack = new Stack<Integer>();
+    		viewflipper.setOutAnimation(AnimationUtils.makeOutAnimation(parent, false));
+    		viewflipper.setInAnimation(AnimationUtils.makeInAnimation(parent, false));
+
+    	}
+    	public void FlipTo(MyView v) {
+    		viewstack.push(viewflipper.getDisplayedChild());
+    		viewflipper.setDisplayedChild(v.id());
+    		
+    	}
+    	public boolean FlipBack() {
+    		
+    		if (viewstack.isEmpty()){
+    			return false;
+    		}
+    		int p = viewstack.pop();
+			viewflipper.setDisplayedChild(p);
+    		return true;
+    	}
+    }
     
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
+        // Read preferences
+        settings = getPreferences(MODE_PRIVATE);
+        TraktApi.Login(settings.getString("username",""),settings.getString("password",""));
+        
         // Save some "handles" to important GUI elements
-        vf = (ViewFlipper) findViewById(R.id.viewFlipper1);
-        vf.setOutAnimation(AnimationUtils.makeOutAnimation(this, false));
-        vf.setInAnimation(AnimationUtils.makeInAnimation(this, false));
+        ViewFlipper vf = (ViewFlipper) findViewById(R.id.viewFlipper1);
+        myflipper = new MyFlipper(vf,this);
+        
         traktlist = new TraktList(this);
         search = new Search(this);
     }
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+	    MenuInflater inflater = getMenuInflater();
+	    inflater.inflate(R.menu.main_menu, menu);
+	    return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    // Handle item selection
+	    switch (item.getItemId()) {
+	    case R.id.menu_settings:
+	    	myflipper.FlipTo(MyView.SETTINGS);
+	    	return true;	    	
+	    default:
+	        return super.onOptionsItemSelected(item);
+	    }
+	}
 	
 	// Override the back button
 	@Override
 	public boolean onKeyDown(int keyCode, KeyEvent event) {
 	    if (keyCode == KeyEvent.KEYCODE_BACK) {
-	        if(vf.getDisplayedChild() != 0){
-	           vf.showPrevious();
+	        if(myflipper.FlipBack()){
 	           return true;
 	        }
 	    }
@@ -55,30 +127,56 @@ public class trakt extends Activity {
 	}
 	
 
+	public void buttonLoginOnClick(View view) {
+		SharedPreferences.Editor ed = settings.edit();
+		TextView un = (TextView)findViewById(R.id.editUsername);
+		TextView pa = (TextView)findViewById(R.id.editPassword);
+		String username = un.getText().toString();
+		String password = pa.getText().toString();
+		try {
+			MessageDigest sha = MessageDigest.getInstance("SHA-1");
+			sha.update(password.getBytes("iso-8859-1"));
+			byte[] hash = sha.digest();
+			password = "";
+			for (int i=0;i<hash.length;i++) {
+				password += Integer.toHexString(hash[i] & 0xFF);
+			}
+		} catch (NoSuchAlgorithmException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		ed.putString("username", username);
+		ed.putString("password", password);
+		ed.commit();
+		TraktApi.Login(username,password);
+	}
 	
     public void buttonTrendingTVOnClick(View view) {
-    	vf.showNext();
+    	myflipper.FlipTo(MyView.TRAKTLIST);
     	TextView tv = (TextView)findViewById(R.id.textTitle);
     	tv.setText("Trending Shows");
     	traktlist.showList("shows/trending.json/%k",false,"Shows",true);
     }
     
     public void buttonTrendingMoviesOnClick(View view) {
-    	vf.showNext();
+    	myflipper.FlipTo(MyView.TRAKTLIST);
     	TextView tv = (TextView)findViewById(R.id.textTitle);
     	tv.setText("Trending Movies");
     	traktlist.showList("movies/trending.json/%k",false,"Movies",true);
     }
     
     public void buttonWatchedOnClick(View view) {
-    	vf.showNext();
+    	myflipper.FlipTo(MyView.TRAKTLIST);;
     	TextView tv = (TextView)findViewById(R.id.textTitle);
-    	tv.setText(Testing.username + " Watched Movies");
-    	traktlist.showList("user/library/movies/all.json/%k/" + Testing.username,true,"Movies",false);
+    	tv.setText(settings.getString("username","") + " Watched Movies");
+    	traktlist.showList("user/library/movies/all.json/%k/" + settings.getString("username",""),true,"Movies",false);
     }
     
     public void buttonSearchSeriesOnClick(View view) {
-    	vf.setDisplayedChild(2);
+    	myflipper.FlipTo(MyView.SEARCH);
     	search.showSearch("Shows");
     }
     
